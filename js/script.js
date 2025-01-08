8a856462b11b57cc00b6eaceae83202a02a8b8c7
C. Saxby Bain <csaxbybain@yahoo.com>
2025-01-08 13:59:21 -0500

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,34 @@
+const statusButton = document.querySelector("button");
+const pets = document.querySelector(".all-pets");
+const createPet = function (name, species) {
+  const pet = {
+    name: name,
+    species: species,
+    isTired: 5,
+    sleep: function () {
+      console.log(`${this.name} needs a nap.`);
+      this.isTired = 1;
+    },
+    play: function () {
+      if (this.isTired === 10) {
+        console.log(`${this.name} is too tired to play.`);
+        this.sleep();
+      } else {
+        console.log(`Yay! ${this.name} loves to play!`);
+        this.isTired += 1;
+      }
+    },
+  };
+  return pet;
+};
+
+const sora = createPet("Sora", "ferret");
+const clover = createPet("Clover", "rabbit");
+const baxter = createPet("Baxter", "hamster");
+const cleo = createPet("Cleo", "rat");
+const francine = createPet("Francine", "turtle");
+
+//console.log(sora, baxter, cleo, francine);
+console.log(clover.sleep());
+console.log(baxter.play());
+console.log(clover, baxter);
