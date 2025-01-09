9130eef3c358c0f4406bdf214bb46fde608e69c4
C. Saxby Bain <csaxbybain@yahoo.com>
2025-01-09 13:23:57 -0500

    Added functions for status button

--- js/script.js
+++ js/script.js
@@ -32,3 +32,27 @@ const francine = createPet("Francine", "turtle");
 //console.log(clover.sleep());
 //console.log(baxter.play());
 //console.log(clover, baxter);
+
+clover.isTired = 8;
+francine.isTired = 9;
+
+const allPets = [sora, clover, baxter, cleo, francine];
+console.log(allPets);
+
+const showPets = function (petArray) {
+  pets.innerHTML = "";
+
+  for (let pet of petArray) {
+    let status = "ready to play!";
+    if (pet.isTired >= 7) {
+      status = "sleeping.";
+    }
+    let li = document.createElement("li");
+    li.innerHTML = `<span class="pet-name">${pet.name} </span> the ${pet.species} is ${status}.`;
+    pets.append(li);
+  }
+};
+
+statusButton.addEventListener("click", function () {
+  showPets(allPets);
+});
